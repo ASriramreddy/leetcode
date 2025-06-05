f2374e919d70f5d67116ddca2ad9622158832d4e
sriram <allugulavenkatreddy.123@gmail.com>
2025-06-05 14:05:25 +0530

    Time: 4 ms (97.28%), Space: 45.1 MB (34.99%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        // Push to minStack if it is empty or val is smaller or equal to current min
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int removed = stack.pop();
+        // Pop from minStack only if the removed element is the minimum
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
